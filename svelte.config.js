1e42ed97dc5d6d02646c7cfbb9a320ea5749e9f0
Sadril Amin Shrestho <sadrilaminshrestho@gmail.com>
2024-01-19 01:32:30 +0600

    vercel Xx

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+
+import adapter from '@sveltejs/adapter-vercel';
+
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
